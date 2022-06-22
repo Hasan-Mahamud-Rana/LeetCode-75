d85d7e8fb9135d0a8581caa9876a7e128235c1f6
Hasan Mahamud Rana <rana_imagine@yahoo.com>
2022-06-22 07:46:49 -0400

    Day 1/ running sum of array

---
+++ Day 1/Running Sum of 1d Array/running-sum-of-1d-array.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
+// Method 1
+var runningSum = function (nums) {
+
+  let ans = new Array(nums.length);
+  ans[0] = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    ans[i] = ans[i - 1] + nums[i];
+  }
+
+  return ans;
+};
+
+
+// Method 2
+var runningSum = function (nums) {
+
+  return nums.map((num, i) => {
+
+    if (i > 0) {
+      for (let j = 0; j < i; j++) {
+        num += nums[j];
+      }
+    }
+    return num;
+
+  });
+
+};
